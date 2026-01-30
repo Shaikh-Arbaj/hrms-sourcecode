414c56154f8378f426a31aa765017ce69fead1ba
Arbaj7378 <arbajshaikh.cs@gmail.com>
2026-01-30 09:28:53 +0530

    changes in script

--- kubeadm_scripts/kubeadm_master.sh
+++ kubeadm_scripts/kubeadm_master.sh
@@ -5,14 +5,12 @@
 # 1. Initialize the Cluster
 echo "Initializing Kubernetes Cluster..."
 sudo kubeadm init
-sleep 2
 
 # 2. Set Up Local kubeconfig
 echo "Setting up local kubeconfig..."
 mkdir -p "$HOME/.kube"
 sudo cp -i /etc/kubernetes/admin.conf "$HOME/.kube/config"
 sudo chown "$(id -u):$(id -g)" "$HOME/.kube/config"
-sleep 2
 
 # 3. Install a Network Plugin (Calico)
 echo "Installing Calico network plugin..."
@@ -21,6 +19,5 @@ kubectl apply -f https://raw.githubusercontent.com/projectcalico/calico/v3.26.0/
 # 4. Generate Join Command
 echo "Generating join command for worker nodes..."
 kubeadm token create --print-join-command
-sleep 2
 
 echo "Kubernetes Master Node setup complete."
